6011d8789124e8fa0c582e7e52438d83c408931d
Axelmarting <axelmarting2002@gmail.com>
2022-07-04 16:06:04 -0300

    nuevo cambio test

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,9 +4,15 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+
+	//nombreIngresado=txtIdNombre.value;
+
+	//El txtIdNombre se saca desde click derecho inspeccionar
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+
+	alert(nombreIngresado);
 
 }
 
 
+//txtIdNombre
